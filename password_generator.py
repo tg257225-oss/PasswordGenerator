f732ab755dd7ad911725a834264b7771e97fa98b
Mapplect <tg257225@gmail.com>
2026-07-19 00:04:06 -0400

    Setting up while loop logic

--- password_generator.py
+++ password_generator.py
@@ -3,4 +3,31 @@ import string
 
 
 def generate_password(min_length, numbers=True, special_characters=True):
-    pass
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_character = random.choice(characters)
+        pwd += new_character
+
+        if new_character in digits:
+            has_number = True
+        elif new_character in special:
+            has_special = True
+
+
+
+generate_password(10)
